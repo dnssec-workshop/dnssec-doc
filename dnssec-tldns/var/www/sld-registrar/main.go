0f8c04917617d2504f3aee34b22b76e0e4d2fb3d
Armin Pech <github@arminpech.de>
2015-12-30 22:03:50 +0100

    sld-registrar: list domain properties with content-type html

--- dnssec-tldns/var/www/sld-registrar/main.go
+++ dnssec-tldns/var/www/sld-registrar/main.go
@@ -139,6 +139,7 @@ func listDomains(w http.ResponseWriter, r *http.Request) {
 
 	data.NameList = domainList
 
+	w.Header().Set("Content-Type", "text/html")
 	err = t.Execute(w, data)
 	if err != nil {
 		errlog.Println("Template execution failed:", err)
