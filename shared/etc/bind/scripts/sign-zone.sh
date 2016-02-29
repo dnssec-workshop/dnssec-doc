f6f56adc218886d9654402f2543b70aef0be7596
Armin Pech <github@arminpech.de>
2016-02-29 20:20:49 +0100

    Fix sign-zone.sh to find zone files and setting names

--- shared/etc/bind/scripts/sign-zone.sh
+++ shared/etc/bind/scripts/sign-zone.sh
@@ -5,19 +5,21 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 ZONEFILE_DIR=/etc/bind/zones
 KEYFILE_DIR=/etc/bind/keys
-RRSIG_VALIDITY="+2h"
-RRSIG_JITTER=300
+RRSIG_VALIDITY=${RRSIG_VALIDITY:-"+2h"}
+RRSIG_JITTER=${RRSIG_JITTER:-"300"}
 
 TLD=$1
+[ "${TLD: -1}" != "." ] && TLD=${TLD}.
 TLD_FILE=$TLD
-[ "$TLD_FILE" = "" ] && TLD_FILE="root"
+[ "$TLD_FILE" = "." ] && TLD_FILE="root"
 
 # Smart zone signing
 if [ ! "$(find $KEYFILE_DIR -name "K${TLD}*" -type f)" ]
 then
-	echo "WARNING: No DNSSEC keys found for zone ${TLD}. key dir $KEYFILE_DIR - signing skipped." >&2
+	echo "WARNING: No DNSSEC keys found for zone '${TLD}' in key dir $KEYFILE_DIR - signing skipped." >&2
 	exit 2
 fi
 
-dnssec-signzone -S -K $KEYFILE_DIR -d $KEYFILE_DIR -e $RRSIG_VALIDITY -j $RRSIG_JITTER -r /dev/urandom -a -3 $(openssl rand 4 -hex) -H 15 -A -o ${TLD}. $ZONEFILE_DIR/${TLD_FILE}.zone
+# Sign the zone and update NSEC3PARAM
+dnssec-signzone -S -K $KEYFILE_DIR -d $KEYFILE_DIR -e $RRSIG_VALIDITY -j $RRSIG_JITTER -r /dev/urandom -a -3 $(openssl rand 4 -hex) -H 15 -A -o ${TLD} $ZONEFILE_DIR/${TLD_FILE}.zone
 exit $?
